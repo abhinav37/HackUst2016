cc25f92313ad93a7de1e6b7ce49b84d0bda705c4
Rachit Jain <Rachit@Rachits-MacBook-Air.local>
2016-04-16 21:08:30 +0800

    userid and redirection ot home.html set

--- html/register.php
+++ html/register.php
@@ -8,11 +8,10 @@ include_once 'dbconnect.php';
 	$lName= mysql_real_escape_string($_POST['lName']);
   $uName = mysql_real_escape_string($_POST['user']);
  
-  $uPass = md5(mysql_real_escape_string($_POST['pass']));
+  $uPass = mysql_real_escape_string($_POST['pass']);
 
  $query = "INSERT INTO user (userID,username,password,firstName,lastName) VALUES ('','$uName','$uPass','$fName','$lName')";
-echo $query;
-
+  
  if(mysqli_query($conn, $query))
  {
 
