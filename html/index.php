16cd443827807276f227354d244140b8457dc61a
Rachit Jain <Rachit@Rachits-MacBook-Air.local>
2016-04-16 20:10:38 +0800

    Login fixed

--- html/index.php
+++ html/index.php
@@ -1,28 +1,27 @@
 <?php
-$conn=mysqli_connect('localhost','root','') or die ('Failed to Connect '.mysqli_error($conn));
-mysqli_select_db($conn,'hackust') or die ('Failed to Access DB'.mysqli_error($conn));
+
+
+include_once("dbconnect.php");
 
 $user = mysql_real_escape_string($_POST['user']);
 $uPass = mysql_real_escape_string($_POST['pass']);
 
-$query = "select * from `user` where `userName` = \"$user\" ";
-echo $query;
+$query = "select * from `user` where `username` = \"$user\" ";
+
 
 $res=mysqli_query($conn, $query) or die ('Failed to query');
 $row=mysqli_fetch_array($res);
-echo $row['password'];
- 
+
  if($row['password']==$uPass)
  {
-	echo "yes";
-	$_SESSION['user'] = $row['userID'];
-	header("Location: home.php");
+	
+
+	$_SESSION['userID']= $row['userID'];
+	echo "Successfully logged in as " . $row['username'];
  }
  else
  {
-?>
-        <script>alert('wrong details');</script>
-<?php
+ 	echo "Cannot log in";
  }
  
 
