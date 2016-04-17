96802fed2dab4a819ef6c2afd19998adb90d09cd
Aman <amangupta96@outlook.com>
2016-04-17 08:56:18 +0800

    commit

--- html/profile.php
+++ html/profile.php
@@ -17,7 +17,7 @@
   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="../bootstrap/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.1.1/Chart.js"></script>
+  <script type="text/javascript" src="Chart.js"></script>
  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <link rel="stylesheet" href="../css/index.css">
 <script>
